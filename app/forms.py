f8e2284f8798a24f67f0020402548f204a98cfe3
Louis-David Coulombe <louisd.coulombe@gmail.com>
2016-05-26 22:10:07 -0400

    Web app update

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import Form
+from wtforms import StringField, DecimalField
+from wtforms.validators import DataRequired
+
+
+class TaskForm(Form):
+    host = StringField('Host', validators=[DataRequired()])
+    user = StringField('User', validators=[DataRequired()])
+    passwd = StringField('Pass', validators=[DataRequired()])
+    task = StringField('Task', validators=[DataRequired()])
+    interval = DecimalField('Interval', validators=[DataRequired()])
+    disks = StringField('Disks')
+    db_host = StringField('Graphite Host', validators=[DataRequired()])
+    db_port = StringField('Graphite Port', validators=[DataRequired()])
+    path = StringField('Graphite Path', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,18 +1,26 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from task_parser import TaskParser
+import os
 
 TASK_DIR = 'task_dir/'
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     taskparser = TaskParser(TASK_DIR)
     taskparser.parse()
-    return render_template('index.html', task_list=taskparser.task_list)
+    return render_template('index.html',
+                           task_list=taskparser.task_list)
 
 
-@app.route('/index/<id>')
+@app.route('/task')
+def task():
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
 def delete(id):
-    print 'deleting ' + id
+    path = os.path.join(TASK_DIR, id + '.yml')
+    print 'deleting ' + path
+    os.remove(path)
+    return redirect(url_for('index'))
